6011b65aa64db6eaf068c69812eeff62468b0d79
suzukaotto <me@szk.kr>
2024-10-24 20:54:57 +0900

    update: Updated code to only fire once when a key is pressed

--- onekey/onekey.ino
+++ onekey/onekey.ino
@@ -1,5 +1,7 @@
 #include <Keyboard.h>
 
+bool isPressed = false;
+
 void setup() {  
   Keyboard.begin();
   pinMode(A2, INPUT_PULLUP);
@@ -12,13 +14,19 @@ void loop() {
   digitalWrite(23, LOW);
   
   if (digitalRead(A2)) {
-    Keyboard.releaseAll();
-    digitalWrite(13, LOW);
+    if (isPressed == true) {
+      isPressed = false;
+      Keyboard.releaseAll();
+      digitalWrite(13, LOW);
+    }
   } else {
-    Keyboard.press(KEY_LEFT_CTRL); // Ctrl 키 누름
-    Keyboard.press('v');            // 'V' 키 누름
-    Keyboard.releaseAll();          // 모든 키 해제
-    digitalWrite(13, HIGH);
+    if (isPressed == false) {
+      isPressed = true;
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('v');
+      Keyboard.releaseAll();
+      digitalWrite(13, HIGH);
+    }
   }
   
   digitalWrite(23, HIGH);
